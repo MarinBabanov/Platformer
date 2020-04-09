b91131ccbfe8a7306610a7323f968fa17d903aac
MarinBabanov <56382191+MarinBabanov@users.noreply.github.com>
2020-04-09 13:46:50 +0200

    First Commit
    - Moving Mechanics
    - Double Jump Mechanics
    - Camera Mechanics
    - Coins/Counter Mechanics

---
+++ Platform-Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerCharacter;
+    public float cameraDistance = 10.0f;
+
+    void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 3.5f) / cameraDistance);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(playerCharacter.position.x, playerCharacter.position.y, transform.position.z);
+    }
+}
